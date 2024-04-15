6155af2bf82e6d39b675b32b52997a3c803713cf
Cesar Acosta <c3sar.adan@gmail.com>
2024-04-15 11:00:37 -0700

    Registrar

--- LoginBackend20241/Controllers/CuentasController.cs
+++ LoginBackend20241/Controllers/CuentasController.cs
@@ -1,7 +1,12 @@
 ﻿using LoginBackend20241.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace LoginBackend20241.Controllers
 {
@@ -33,6 +38,48 @@ namespace LoginBackend20241.Controllers
             var claimsRoles = await userManager.GetClaimsAsync(usuario!);
 
             claims.AddRange(claimsRoles);
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["LlaveJWT"]!));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.Now.AddDays(1);
+
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, 
+                claims: claims, expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiracion = expiracion,
+            };
+        }
+
+        [HttpGet("RenovarToken")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<RespuestaAutenticacion>> Renovar()
+        {
+            var emailClaim = HttpContext.User.Claims.Where(x => x.Type == "email").FirstOrDefault();
+            var credencialesUsuario = new CredencialesUsuario()
+            {
+                Email = emailClaim!.Value
+            };
+            return await ConstruirToken(credencialesUsuario);
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+        {
+            var usuario = new IdentityUser
+            {
+                UserName = credencialesUsuario.Email,
+                Email = credencialesUsuario.Email
+            };
+
+            var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
+            if (resultado.Succeeded)
+                return await ConstruirToken(credencialesUsuario);
+
+            return BadRequest(resultado.Errors);
         }
     }
 }
